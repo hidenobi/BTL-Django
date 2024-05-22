b99a7fbb6c3fe4e8f021a188149c59692f3021b7
TuanVH <dungtuan095@gmail.com>
2024-05-22 19:42:33 +0700

    doi ten shop

--- UserService/UserService/settings.py
+++ UserService/UserService/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.mysql",
         "NAME": "user_service",
         "USER": "root",
-        "PASSWORD": "1234",
+        "PASSWORD": "12345678",
         "HOST": "127.0.0.1",
         "PORT": "3306",
     }
